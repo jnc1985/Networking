08865b9a8024446067578d9b112a846d81aeb04b
jnc1985 <90096890+jnc1985@users.noreply.github.com>
2021-09-10 20:03:58 -0400

    Update solution.py
    6th - Attempt

--- solution.py
+++ solution.py
@@ -1,13 +1,10 @@
 ### welcome_assignment_answers
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
-answer = ''
-
 
 def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
-    global answer
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = str("No")
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
