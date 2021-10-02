e242cd0ff170a9146cc1b37b8fa687e2789c16ce
jnc1985 <90096890+jnc1985@users.noreply.github.com>
2021-10-02 11:01:03 -0400

    Update solution.py
    Attempt 8 - Capitalized Word

--- solution.py
+++ solution.py
@@ -30,7 +30,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Send MAIL FROM command and print server response.
     # Fill in start
     #print('Sending E-mail...')
-    mailpackage = "MAIL From: jnc5@nyu.edu\r\n"
+    mailpackage = "MAIL FROM: jnc5@nyu.edu\r\n"
     clientSocket.send(mailpackage.encode())
     recv2 = clientSocket.recv(1024).decode()
     #print(recv2)
