654fb67fa90b6533bdc9037986df994ab2b29b24
jnc1985 <90096890+jnc1985@users.noreply.github.com>
2021-11-20 10:41:46 -0500

    Update - 11

--- solution.py
+++ solution.py
@@ -134,15 +134,13 @@ def get_route(hostname):
 
                 if types == 11:
                     bytes = struct.calcsize("d")
-                    timeSent = struct.unpack("d", recvPacket[28:28 +
-                                                                bytes])[0]
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
                     tracelist1 = [ttl, rtt, addr[0], hname]
                     tracelist2.append(tracelist1)
                     print(tracelist1)
                     print(tracelist2)
-                    return tracelist2
                     # Fill in end
                 elif types == 3:
                     bytes = struct.calcsize("d")
@@ -153,7 +151,6 @@ def get_route(hostname):
                     tracelist2.append(tracelist1)
                     print(tracelist1)
                     print(tracelist2)
-                    return tracelist2
                     # Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
@@ -174,7 +171,6 @@ def get_route(hostname):
                     tracelist2.append(tracelist1)
                     print(tracelist1)
                     print(tracelist2)
-                    return tracelist2
                     # Fill in end
                 break
             finally:
