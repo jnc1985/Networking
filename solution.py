52f1ea6c40b7aaeca5ebc71aa7aa0e2349667bd6
jnc1985 <90096890+jnc1985@users.noreply.github.com>
2021-11-20 20:05:25 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -134,27 +134,33 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1 = [ttl, rtt, addr[0], hname]
-                    tracelist2.append(str(tracelist1))
-                    print(tracelist1)
+                    #tracelist1 = [ttl, rtt, addr[0], hname]
+                    #tracelist2.append(str(tracelist1))
+                    #print(tracelist1)
+                    response_test = [str(ttl),str((timeReceived-t)*1000)+'ms',str(addr[0]),str(hname)]
+					tracelist2.append(response_test)
                     # Fill in end
                 elif types == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1 = [ttl, rtt, addr[0], hname]
-                    tracelist2.append(str(tracelist1))
-                    print(tracelist1)
+                    #tracelist1 = [ttl, rtt, addr[0], hname]
+                    #tracelist2.append(str(tracelist1))
+                    #print(tracelist1)
+                    response_test = [str(ttl),str((timeReceived-t)*1000)+'ms',str(addr[0]),str(hname)]
+					tracelist2.append(response_test)
                     # Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1 = [ttl, rtt, addr[0], hname]
-                    tracelist2.append(str(tracelist1))
-                    print(tracelist1)
+                    #tracelist1 = [ttl, rtt, addr[0], hname]
+                    #tracelist2.append(str(tracelist1))
+                    #print(tracelist1)
+                    response_test = [str(ttl),str((timeReceived-t)*1000)+'ms',str(addr[0]),str(hname)]
+					tracelist2.append(response_test)
                     if hname == addr[0]:
                         return tracelist2
                     # Fill in end
@@ -163,7 +169,7 @@ def get_route(hostname):
                     # If there is an exception/error to your if statements, you should append that to your list here
                     tracelist1.append("ERROR")
                     tracelist2.append(str(tracelist1))
-                    print(tracelist1)
+                    #print(tracelist1)
                     # Fill in end
                 break
             finally:
